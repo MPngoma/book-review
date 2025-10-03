3df1d5d78a84e0cb28326a09391f95858a2053e7
Milton <milto@LAPTOP2021EBS.com>
2025-10-02 23:27:09 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book review",
+    password: "Migloleoj04!",
+    port: 5432,
+})
+db.connect();
+
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// async function fetchBooks() {
+//     const response = await axios.get('https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=10');
+//     return response.data.items;
+// }
+
+async function log_books() {
+    const result = await db.query("SELECT * FROM books");
+    return result.rows;
+}
+
+
+app.get('/', async (req, res) => {
+    const books = await log_books();
+    res.render('index', { books });
+    console.log(books);
+});
+
+app.get('/review', (req, res) => {
+    res.render('review');});
+
+app.post('/submit-review', async (req, res) => {
+    const { title, author, review, isbn, rating } = req.body;
+    try {
+        const result = await db.query(
+            "INSERT INTO books (title, author, review, isbn, rating) VALUES ($1, $2, $3, $4, $5)",
+            [title, author, review, isbn, rating]
+        );
+        console.log('Review submitted:', result.rows[1]);
+    }
+    catch (error) {
+        console.error('Error submitting review:', error);
+        res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
